c27243940fb4a17c155e08553996452fc2ccc058
Refodeus <Romylik22R@yandex.ru>
2024-07-08 01:36:05 +0300

    and more designed a map

--- Assets/Scripts/MovingParallax.cs
+++ Assets/Scripts/MovingParallax.cs
@@ -9,6 +9,9 @@ public class MovingParallax : MonoBehaviour
     public Transform backBG;
     public float length = 47.5f;
     public float range = 0f;
+
+    int ratio = 1;
+    float additionalRatio = 2.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +21,19 @@ public class MovingParallax : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (mainCam.position.x + range > middleBG.position.x)
-            backBG.position = middleBG.position + Vector3.right * length;
-        if (mainCam.position.x + range < middleBG.position.x)
+        if (mainCam.position.x + range > middleBG.position.x * ratio  * additionalRatio)
+        {
+            if (mainCam.position.x + range > middleBG.position.x * (ratio + 1)  * additionalRatio)
+                ratio++;
+            Debug.Log($"ratio={ratio}, mainCam={mainCam.position.x}, range*(ratio + 1)={range * (ratio + 1)}, middleBG={middleBG.position.x}");
+            backBG.position = middleBG.position + Vector3.right * length * ratio;
+        }
+        if (mainCam.position.x + range * ratio < middleBG.position.x)
+        {
+            Debug.Log("DOWNER" + "ratio =  " + ratio + "     " + mainCam.position.x);
+            if (mainCam.position.x + range * (ratio + 1) < middleBG.position.x)
+                ratio--;
             backBG.position = middleBG.position + Vector3.left * length;
+        }
     }
 }
