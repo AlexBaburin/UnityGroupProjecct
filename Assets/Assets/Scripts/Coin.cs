aba6efb8084e3c288394d6129d046de546264f24
Refodeus <Romylik22R@yandex.ru>
2024-07-08 23:46:03 +0300

    FINALE OF FINAL GAME
    baburka

--- Assets/Assets/Scripts/Coin.cs
+++ Assets/Assets/Scripts/Coin.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     static public int coins = 0;
+    private void Awake()
+    {
+        coins = 0;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
